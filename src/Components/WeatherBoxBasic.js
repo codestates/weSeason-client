36d5be2608a82d211fc2018171b3e6d0508977b5
youme <mrsimplelife@naver.com>
2021-01-01 07:48:28 +0900

    [client] Add clothes, weather 기본틀

--- src/Components/WeatherBoxBasic.js
+++ src/Components/WeatherBoxBasic.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WeatherItem from "./WeatherItem";
 import "./WeatherBoxBasic.css";
 
-const WeatherBoxBasic = ({ weatherData, accessToken }) => {
+const WeatherBoxBasic = ({ accessToken, lat, lon, clickWeather }) => {
+  const [weatherData, setWeatherData] = useState([]);
+  //lat or lon 바뀔 때마다 실행합니다.
+  useEffect(() => {
+    // 벡엔드에 lat,lon 이용해서 요청, 응답온거 weatherData 상태저장 하시고 사용합니다.
+    // accessToken 없으면 header 제외하고 요청하면 벡엔드에서 체감온도 없이 옵니다.
+  }, [lat, lon]);
   let weatherList;
-
   if (!accessToken) {
     weatherList = weatherData.map((weather) => {
       return (
         <WeatherItem
+          // 유저가 각각의 날씨아이템을 클리하면 메인에 클릭한 해당 온도 상태저장 되게 해주세요.
+          clickWeather={clickWeather}
           key={weather.dt.toString()}
           hour={weather.dt}
           temp={weather.temp}
